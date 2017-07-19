a338e839ece4a41b861f295cbd0b0c66a400c568
Jackson Graziano <jackgraziano@gmail.com>
2017-07-19 16:53:15 -0300

    folders

--- lib/mapas.rb
+++ lib/mapas.rb
@@ -19,36 +19,36 @@ end
 
 def make_gifs
   animation = ImageList.new(
-                            "data/mapas/eta/shad50_01.gif",
-                            "data/mapas/eta/shad50_02.gif",
-                            "data/mapas/eta/shad50_03.gif",
-                            "data/mapas/eta/shad50_04.gif",
-                            "data/mapas/eta/shad50_05.gif",
-                            "data/mapas/eta/shad50_06.gif",
-                            "data/mapas/eta/shad50_07.gif",
-                            "data/mapas/eta/shad50_08.gif",
-                            "data/mapas/eta/shad50_09.gif",
-                            "data/mapas/eta/shad50_10.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_01.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_02.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_03.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_04.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_05.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_06.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_07.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_08.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_09.gif",
+                            "#{Dir.pwd}/data/mapas/eta/shad50_10.gif",
                             )
   animation.delay = 100
   animation.iterations = 0
-  animation.write("data/send/gif_eta.gif")
+  animation.write("#{Dir.pwd}/data/send/gif_eta.gif")
 
   animation = ImageList.new(
-                            "data/mapas/global/glob50_01.gif",
-                            "data/mapas/global/glob50_02.gif",
-                            "data/mapas/global/glob50_03.gif",
-                            "data/mapas/global/glob50_04.gif",
-                            "data/mapas/global/glob50_05.gif",
-                            "data/mapas/global/glob50_06.gif",
-                            "data/mapas/global/glob50_07.gif",
-                            "data/mapas/global/glob50_08.gif",
-                            "data/mapas/global/glob50_09.gif",
-                            "data/mapas/global/glob50_10.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_01.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_02.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_03.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_04.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_05.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_06.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_07.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_08.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_09.gif",
+                            "#{Dir.pwd}/data/mapas/global/glob50_10.gif",
                             )
   animation.delay = 100
   animation.iterations = 0
-  animation.write("data/send/gif_global.gif")
+  animation.write("#{Dir.pwd}/data/send/gif_global.gif")
 end
 
 def download_gefs
