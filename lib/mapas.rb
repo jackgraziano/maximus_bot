be7b7bbd96a59c6c55eebb2f55ac68ee4eb63c17
Jackson Graziano <jackgraziano@gmail.com>
2017-07-19 17:02:40 -0300

    fix the shit out

--- lib/mapas.rb
+++ lib/mapas.rb
@@ -1,8 +1,8 @@
 include Magick
 
 def clean_maps
-  FileUtils.rm_rf(Dir.glob('#{Dir.pwd}/data/mapas/eta/*'))
-  FileUtils.rm_rf(Dir.glob('#{Dir.pwd}/data/mapas/global/*'))
+  FileUtils.rm_rf(Dir.glob('#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/*'))
+  FileUtils.rm_rf(Dir.glob('#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/*'))
 end
 
 def download_eta
@@ -10,7 +10,7 @@ def download_eta
     url = "/images/operacao_integrada/meteorologia/eta/shad50_#{n}.gif"
     Net::HTTP.start("www.ons.org.br") { |http|
       resp = http.get(url)
-      open("#{Dir.pwd}/data/mapas/eta/shad50_#{n.to_s.rjust(2,"0")}.gif", "wb") { |file|
+      open("#{File.expand_path File.dirname(__FILE__)}}/code/maximus_bot/data/mapas/eta/shad50_#{n.to_s.rjust(2,"0")}.gif", "wb") { |file|
         file.write(resp.body)
        }
     }
@@ -19,36 +19,36 @@ end
 
 def make_gifs
   animation = ImageList.new(
-                            "#{Dir.pwd}/data/mapas/eta/shad50_01.gif",
-                            "#{Dir.pwd}/data/mapas/eta/shad50_02.gif",
-                            "#{Dir.pwd}/data/mapas/eta/shad50_03.gif",
-                            "#{Dir.pwd}/data/mapas/eta/shad50_04.gif",
-                            "#{Dir.pwd}/data/mapas/eta/shad50_05.gif",
-                            "#{Dir.pwd}/data/mapas/eta/shad50_06.gif",
-                            "#{Dir.pwd}/data/mapas/eta/shad50_07.gif",
-                            "#{Dir.pwd}/data/mapas/eta/shad50_08.gif",
-                            "#{Dir.pwd}/data/mapas/eta/shad50_09.gif",
-                            "#{Dir.pwd}/data/mapas/eta/shad50_10.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_01.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_02.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_03.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_04.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_05.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_06.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_07.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_08.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_09.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/eta/shad50_10.gif",
                             )
   animation.delay = 100
   animation.iterations = 0
-  animation.write("#{Dir.pwd}/data/send/gif_eta.gif")
+  animation.write("#{File.expand_path File.dirname(__FILE__)}}/data/send/gif_eta.gif")
 
   animation = ImageList.new(
-                            "#{Dir.pwd}/data/mapas/global/glob50_01.gif",
-                            "#{Dir.pwd}/data/mapas/global/glob50_02.gif",
-                            "#{Dir.pwd}/data/mapas/global/glob50_03.gif",
-                            "#{Dir.pwd}/data/mapas/global/glob50_04.gif",
-                            "#{Dir.pwd}/data/mapas/global/glob50_05.gif",
-                            "#{Dir.pwd}/data/mapas/global/glob50_06.gif",
-                            "#{Dir.pwd}/data/mapas/global/glob50_07.gif",
-                            "#{Dir.pwd}/data/mapas/global/glob50_08.gif",
-                            "#{Dir.pwd}/data/mapas/global/glob50_09.gif",
-                            "#{Dir.pwd}/data/mapas/global/glob50_10.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_01.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_02.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_03.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_04.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_05.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_06.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_07.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_08.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_09.gif",
+                            "#{File.expand_path File.dirname(__FILE__)}}/data/mapas/global/glob50_10.gif",
                             )
   animation.delay = 100
   animation.iterations = 0
-  animation.write("#{Dir.pwd}/data/send/gif_global.gif")
+  animation.write("#{File.expand_path File.dirname(__FILE__)}}/data/send/gif_global.gif")
 end
 
 def download_gefs
@@ -56,7 +56,7 @@ def download_gefs
     url = "/images/operacao_integrada/meteorologia/global/glob50_#{n}.gif"
     Net::HTTP.start("www.ons.org.br") { |http|
       resp = http.get(url)
-      open("data/mapas/global/glob50_#{n.to_s.rjust(2,"0")}.gif", "wb") { |file|
+      open("#{File.expand_path File.dirname(__FILE__)}}/code/maximus_bot/data/mapas/global/glob50_#{n.to_s.rjust(2,"0")}.gif", "wb") { |file|
         file.write(resp.body)
        }
     }
@@ -64,8 +64,8 @@ def download_gefs
 end
 
 def make_pdf
-  pdf = WickedPdf.new.pdf_from_html_file("#{Dir.pwd}/data/mapas/mapas.html", orientation: 'Landscape')
-  File.open('data/send/mapas.pdf', 'wb') do |file|
+  pdf = WickedPdf.new.pdf_from_html_file("#{File.expand_path File.dirname(__FILE__)}}/data/mapas/mapas.html", orientation: 'Landscape')
+  File.open("File.expand_path File.dirname(__FILE__)}/data/send/mapas.pdf", 'wb') do |file|
     file << pdf
   end
 end
